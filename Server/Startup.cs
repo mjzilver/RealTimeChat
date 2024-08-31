9f256560af18dfbc90260eaa96c3c5c627838185
mjzilver <jari.zilverentant@gmail.com>
2024-08-31 16:22:34 +0200

    moved websockets to middleware

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -3,8 +3,7 @@ using B4mServer.Websockets;
 using B4mServer.Websockets.Interfaces;
 using Microsoft.AspNetCore.WebSockets;
 using Microsoft.EntityFrameworkCore;
-using System.Net;
-using System.Net.WebSockets;
+using B4mServer.Middleware;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -29,10 +28,10 @@ public class Startup
 		services.AddSingleton<IMemoryStore, MemoryStore>();
 		services.AddSingleton<IWebSocketSender, WebSocketSender>();
 		// Command processors
-		services.AddScoped<IUserCommandProcessor, UserCommandProcessor>();
-		services.AddScoped<IChannelCommandProcessor, ChannelCommandProcessor>();
-		services.AddScoped<IMessageCommandProcessor, MessageCommandProcessor>();
-		services.AddScoped<IWebSocketCommandProcessor, WebSocketCommandProcessor>();
+		services.AddSingleton<IUserCommandProcessor, UserCommandProcessor>();
+		services.AddSingleton<IChannelCommandProcessor, ChannelCommandProcessor>();
+		services.AddSingleton<IMessageCommandProcessor, MessageCommandProcessor>();
+		services.AddSingleton<IWebSocketCommandProcessor, WebSocketCommandProcessor>();
 
 		services.AddWebSockets(options =>
 		{
@@ -57,29 +56,6 @@ public class Startup
 			endpoints.MapControllers();
 		});
 
-		app.Use(async (context, next) =>
-		{
-			if (context.Request.Path == "/ws")
-			{
-				if (context.WebSockets.IsWebSocketRequest)
-				{
-					WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
-					IWebSocketCommandProcessor commandProcessor = context.RequestServices.GetRequiredService<IWebSocketCommandProcessor>();
-					IMemoryStore memoryStore = context.RequestServices.GetRequiredService<IMemoryStore>();
-					JsonSerializerOptions options = context.RequestServices.GetRequiredService<JsonSerializerOptions>();
-
-					WebSocketHandler handler = new(webSocket, commandProcessor, memoryStore, options);
-					await handler.Handle();
-				}
-				else
-				{
-					context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-				}
-			}
-			else
-			{
-				await next();
-			}
-		});
+		app.UseMiddleware<CustomWebSocketMiddleware>();
 	}
 }
