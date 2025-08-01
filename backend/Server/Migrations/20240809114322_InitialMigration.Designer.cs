mjzilver/B4m-Server

Rename to RealTimeServer

backend/Server/Migrations/20240809114322_InitialMigration.Designer.cs
@@ -1,6 +1,6 @@
 ﻿// <auto-generated />
 using System;
-using B4mServer.Data;
+using RealTimeServerServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace B4mServer.Migrations
+namespace RealTimeServerServer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
     [Migration("20240809114322_InitialMigration")]
@@ -20,7 +20,7 @@ namespace B4mServer.Migrations
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.7");
 
-            modelBuilder.Entity("B4mServer.Models.Channel", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Channel", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -52,7 +52,7 @@ namespace B4mServer.Migrations
                     b.ToTable("Channels");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Message", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Message", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -81,7 +81,7 @@ namespace B4mServer.Migrations
                     b.ToTable("Messages");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.User", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.User", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -108,9 +108,9 @@ namespace B4mServer.Migrations
                     b.ToTable("Users");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Channel", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Channel", b =>
                 {
-                    b.HasOne("B4mServer.Models.User", "Owner")
+                    b.HasOne("RealTimeServerServer.Models.User", "Owner")
                         .WithMany("OwnedChannels")
                         .HasForeignKey("OwnerId")
                         .OnDelete(DeleteBehavior.Restrict);
@@ -118,15 +118,15 @@ namespace B4mServer.Migrations
                     b.Navigation("Owner");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Message", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Message", b =>
                 {
-                    b.HasOne("B4mServer.Models.Channel", "Channel")
+                    b.HasOne("RealTimeServerServer.Models.Channel", "Channel")
                         .WithMany("Messages")
                         .HasForeignKey("ChannelId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-                    b.HasOne("B4mServer.Models.User", "User")
+                    b.HasOne("RealTimeServerServer.Models.User", "User")
                         .WithMany("Messages")
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade)
@@ -137,12 +137,12 @@ namespace B4mServer.Migrations
                     b.Navigation("User");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Channel", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Channel", b =>
                 {
                     b.Navigation("Messages");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.User", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.User", b =>
                 {
                     b.Navigation("Messages");
 

backend/Server/Migrations/AppDbContextModelSnapshot.cs
@@ -1,13 +1,13 @@
 ﻿// <auto-generated />
 using System;
-using B4mServer.Data;
+using RealTimeServerServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace B4mServer.Migrations
+namespace RealTimeServerServer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
     partial class AppDbContextModelSnapshot : ModelSnapshot
@@ -17,7 +17,7 @@ namespace B4mServer.Migrations
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.7");
 
-            modelBuilder.Entity("B4mServer.Models.Channel", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Channel", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -49,7 +49,7 @@ namespace B4mServer.Migrations
                     b.ToTable("Channels");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Message", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Message", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -78,7 +78,7 @@ namespace B4mServer.Migrations
                     b.ToTable("Messages");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.User", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.User", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -105,9 +105,9 @@ namespace B4mServer.Migrations
                     b.ToTable("Users");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Channel", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Channel", b =>
                 {
-                    b.HasOne("B4mServer.Models.User", "Owner")
+                    b.HasOne("RealTimeServerServer.Models.User", "Owner")
                         .WithMany("OwnedChannels")
                         .HasForeignKey("OwnerId")
                         .OnDelete(DeleteBehavior.Restrict);
@@ -115,15 +115,15 @@ namespace B4mServer.Migrations
                     b.Navigation("Owner");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Message", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Message", b =>
                 {
-                    b.HasOne("B4mServer.Models.Channel", "Channel")
+                    b.HasOne("RealTimeServerServer.Models.Channel", "Channel")
                         .WithMany("Messages")
                         .HasForeignKey("ChannelId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-                    b.HasOne("B4mServer.Models.User", "User")
+                    b.HasOne("RealTimeServerServer.Models.User", "User")
                         .WithMany("Messages")
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade)
@@ -134,12 +134,12 @@ namespace B4mServer.Migrations
                     b.Navigation("User");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Channel", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Channel", b =>
                 {
                     b.Navigation("Messages");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.User", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.User", b =>
                 {
                     b.Navigation("Messages");
 

mjzilver/RealTimeChat

backend/Server/Migrations/20240809114322_InitialMigration.Designer.cs
@@ -1,16 +1,16 @@
 ﻿// <auto-generated />
 using System;
-using B4mServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using RealTimeChatServer.Data;
 
 #nullable disable
 
-namespace B4mServer.Migrations
+namespace RealTimeServerServer.Migrations
 {
-    [DbContext(typeof(AppDbContext))]
+	[DbContext(typeof(AppDbContext))]
     [Migration("20240809114322_InitialMigration")]
     partial class InitialMigration
     {
@@ -20,7 +20,7 @@ namespace B4mServer.Migrations
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.7");
 
-            modelBuilder.Entity("B4mServer.Models.Channel", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Channel", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -52,7 +52,7 @@ namespace B4mServer.Migrations
                     b.ToTable("Channels");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Message", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Message", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -81,7 +81,7 @@ namespace B4mServer.Migrations
                     b.ToTable("Messages");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.User", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.User", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -108,9 +108,9 @@ namespace B4mServer.Migrations
                     b.ToTable("Users");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Channel", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Channel", b =>
                 {
-                    b.HasOne("B4mServer.Models.User", "Owner")
+                    b.HasOne("RealTimeServerServer.Models.User", "Owner")
                         .WithMany("OwnedChannels")
                         .HasForeignKey("OwnerId")
                         .OnDelete(DeleteBehavior.Restrict);
@@ -118,15 +118,15 @@ namespace B4mServer.Migrations
                     b.Navigation("Owner");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Message", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Message", b =>
                 {
-                    b.HasOne("B4mServer.Models.Channel", "Channel")
+                    b.HasOne("RealTimeServerServer.Models.Channel", "Channel")
                         .WithMany("Messages")
                         .HasForeignKey("ChannelId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-                    b.HasOne("B4mServer.Models.User", "User")
+                    b.HasOne("RealTimeServerServer.Models.User", "User")
                         .WithMany("Messages")
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade)
@@ -137,12 +137,12 @@ namespace B4mServer.Migrations
                     b.Navigation("User");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Channel", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Channel", b =>
                 {
                     b.Navigation("Messages");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.User", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.User", b =>
                 {
                     b.Navigation("Messages");
 

backend/Server/Migrations/AppDbContextModelSnapshot.cs
@@ -1,15 +1,15 @@
 ﻿// <auto-generated />
 using System;
-using B4mServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using RealTimeChatServer.Data;
 
 #nullable disable
 
-namespace B4mServer.Migrations
+namespace RealTimeServerServer.Migrations
 {
-    [DbContext(typeof(AppDbContext))]
+	[DbContext(typeof(AppDbContext))]
     partial class AppDbContextModelSnapshot : ModelSnapshot
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
@@ -17,7 +17,7 @@ namespace B4mServer.Migrations
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.7");
 
-            modelBuilder.Entity("B4mServer.Models.Channel", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Channel", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -49,7 +49,7 @@ namespace B4mServer.Migrations
                     b.ToTable("Channels");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Message", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Message", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -78,7 +78,7 @@ namespace B4mServer.Migrations
                     b.ToTable("Messages");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.User", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.User", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -105,9 +105,9 @@ namespace B4mServer.Migrations
                     b.ToTable("Users");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Channel", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Channel", b =>
                 {
-                    b.HasOne("B4mServer.Models.User", "Owner")
+                    b.HasOne("RealTimeServerServer.Models.User", "Owner")
                         .WithMany("OwnedChannels")
                         .HasForeignKey("OwnerId")
                         .OnDelete(DeleteBehavior.Restrict);
@@ -115,15 +115,15 @@ namespace B4mServer.Migrations
                     b.Navigation("Owner");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Message", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Message", b =>
                 {
-                    b.HasOne("B4mServer.Models.Channel", "Channel")
+                    b.HasOne("RealTimeServerServer.Models.Channel", "Channel")
                         .WithMany("Messages")
                         .HasForeignKey("ChannelId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-                    b.HasOne("B4mServer.Models.User", "User")
+                    b.HasOne("RealTimeServerServer.Models.User", "User")
                         .WithMany("Messages")
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade)
@@ -134,12 +134,12 @@ namespace B4mServer.Migrations
                     b.Navigation("User");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.Channel", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.Channel", b =>
                 {
                     b.Navigation("Messages");
                 });
 
-            modelBuilder.Entity("B4mServer.Models.User", b =>
+            modelBuilder.Entity("RealTimeServerServer.Models.User", b =>
                 {
                     b.Navigation("Messages");
 